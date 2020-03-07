b45b7e2c9902a0f14e750f5a87fc4509dbd2a17f
bhanujoshi24 <51364844+bhanujoshi24@users.noreply.github.com>
2020-03-07 13:22:59 +0530

    Add files via upload

---
+++ Blog/mysite/blog/admin.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from .models import Post, Comment
+from django.contrib import admin
+from django.contrib.auth.models import User
+admin.site.unregister(User)
+admin.site.register(User)
+# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
